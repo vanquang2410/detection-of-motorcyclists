3d69f826f9d51d72b519a81346ff284f2c0ff912
vanquang <nguyenvanquangq013@gmail.com>
2025-02-17 16:02:17 +0000

    init

---
+++ open-learn-hub/core-utils/src/main/java/com/olh/core_utils/CoreUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.olh.core_utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreUtilsApplication.class, args);
+	}
+
+}
---
+++ open-learn-hub/core-utils/src/test/java/com/olh/core_utils/CoreUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olh.core_utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
